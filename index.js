fd505f22791c84bcdf044ffe998ffabe59895f05
MEVINDANI <mevindani14@gmail.com>
2024-03-23 14:05:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import Form from './Form'
+import { name as appName } from './app.json';
+import TaskManagerHome from './components/TaskManagerHome';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => TaskManagerHome);
